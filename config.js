a1e31c1795860c767e223eb9949bc7f1aa021d25
MaRunmin <marunmin@marunmins-mbp.fc.ca>
2018-03-12 13:46:41 -0400

    this is the first homework

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+  user : "root",
+  password : "hopalong", // leave this blank for windows machines
+  port: 3306,
+  host: "localhost",
+  database: "NEH_lab",
+
+  kidsmode : false
+}
+
+module.exports = config;
